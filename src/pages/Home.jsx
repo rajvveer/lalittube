c32d94bcdf508b1c4e0d5a142907ad7e2985be5a
rajveer <rajveershekhawat626@gmail.com>
2023-01-16 15:47:52 +0530

    Ready to deploy

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import  { useEffect, useState } from "react";
+import Card from "../components/Card";
+import axios from "axios";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = ({type}) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get(`/video/${type}`);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, [type]);
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card key={video._id} video={video}/>
+      ))}
+    </Container>
+  );
+};
+
+export default Home;
